60741fd723ce678684ac44437df3e95269b49e19
ekelley9 <evan.kelley@comcast.net>
2020-05-16 18:44:17 -0700

    Fixed typos and added the Short Answer table with the required add and pulling methods

--- TeamTriviaMaze/src/SQLDatabase.java
+++ TeamTriviaMaze/src/SQLDatabase.java
@@ -3,13 +3,13 @@ import java.util.*;
 
 public class SQLDatabase {
 	
-	//Fields for the SQLDatabase object setting up a connection and statement objects to properly use SQL commands
+	//Fields for the SQLDatabase object setting up a Connection, Statement, and ResultSet object to properly use SQL commands
 	private Connection curConection = null;
 	private Statement curStatement = null;
 	private ResultSet queryResult = null;
 	private Random rand = new Random();
 	
-	//Constructor sets the connection to the database TriviQuestionsDB
+	//Constructor sets the connection to the database TriviQuestionsDB.db
 	public SQLDatabase() {
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -22,7 +22,7 @@ public class SQLDatabase {
 		System.out.println("Table made correctly");
 	}
 	
-	//adds a Multiple choice tuple to the MultipleChoice table in the current database ADMIN USE ONLY
+	//Adds a Multiple choice question to the MultipleChoice table in the current database ADMIN USE ONLY
 	public void addMultipleChoice(Scanner input){
 		String question = "";
 		String wrongAnswer1 = "";
@@ -63,7 +63,7 @@ public class SQLDatabase {
 		}
 	}
 	
-	// adds a True False question to the TrueFalse table in the current database ADMIN USE ONLY
+	//Adds a True False question to the TrueFalse table in the current database ADMIN USE ONLY
 	public void addTrueFalse(Scanner input) {
 		String question = "";
 		String correctAnswer = "";
@@ -84,7 +84,7 @@ public class SQLDatabase {
 				submit = false;
 		}while(submit); 
 		
-		toInsert = "Insert into MultipleChoice (QUESTION, WRONG_ANSWER1, WRONG_ANSWER2, CORRECT_ANSWER)"
+		toInsert = "Insert into TrueFalse (QUESTION, CORRECT_ANSWER)"
 				+ "Values(" + "\"" +question + "\", " + "\"" + correctAnswer+"\");";
 		
 		
@@ -97,7 +97,41 @@ public class SQLDatabase {
 		}
 	}
 	
-	//gets a random tuple from the Multiple Choice table in the current database returns it as a String ArrayList
+	//Adds a Short Answer question to the ShortAnswer table in the current database ADMIN USE ONLY
+	public void addShortAnswer(Scanner input) {
+		String question = "";
+		String correctAnswer = "";
+		String toInsert = "";
+		boolean submit = true;
+		
+		do { 
+			System.out.print("Enter the question you'd like to add: ");
+			question = input.nextLine();
+			
+			System.out.print("\nEnter the correct answer: ");
+			correctAnswer = input.nextLine();
+			
+			System.out.print("\nYour query is Quesiton: " + ", Correct Answer: " + correctAnswer
+					+"\nIs that correct? \n1. yes \n2.no: ");
+			
+			if(input.nextLine().equalsIgnoreCase("1"))
+				submit = false;
+		}while(submit); 
+		
+		toInsert = "Insert into ShortAnswer (QUESTION, CORRECT_ANSWER)"
+				+ "Values(" + "\"" +question + "\", " + "\"" + correctAnswer+"\");";
+		
+		
+		try {
+			this.curStatement.executeUpdate(toInsert);
+			System.out.println("New tuple successfully inserted!");
+			
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	//Gets a random tuple from the Multiple Choice table in the current database returns it as a String ArrayList
 	//in the form of ArrayList[0] = is the actual question ArrayList[1-2] = the wrong answers ArrayList[3] = is the correct answer
 	public ArrayList<String> getMultipleChoice() {
 		ArrayList<String> questionArray = new ArrayList<String>();
@@ -139,7 +173,7 @@ public class SQLDatabase {
 		return questionArray;
 	}
 	
-	//gets a random tuple from the True False table in the current database returns it as a string array
+	//Gets a random tuple from the True False table in the current database returns it as a string array
 	//in the form of ArrayList[0] = is the actual question and ArrayList[1] = is the correct answer false or true*
 	public ArrayList<String> getTrueFalse() {
 		ArrayList<String> questionArray = new ArrayList<String>();
@@ -148,6 +182,39 @@ public class SQLDatabase {
 		String correctAnswer = "";
 		String query = "Select QUESTION, CORRECT_ANSWER"
 				+ " from TrueFalse "
+				+ "where QUESTION_ID = ";
+		
+		try {
+			this.queryResult = this.curStatement.executeQuery("Select max(QUESTION_ID) from TrueFalse");
+		
+			queryToReturn = this.rand.nextInt(queryResult.getInt("QUESTION_ID"))+1;
+			
+			this.queryResult = this.curStatement.executeQuery(query+queryToReturn+";");
+			
+			while(this.queryResult.next()) {
+				question = this.queryResult.getString("QUESTION");
+				correctAnswer = this.queryResult.getString("CORRECT_ANSWER");
+			}
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		questionArray.add(question);
+		questionArray.add(correctAnswer);
+		
+		return questionArray;
+	
+	}
+	
+	//Gets a random tuple from the True False table in the current database returns it as a string array
+	//in the form of ArrayList[0] = is the actual question and ArrayList[1] = is the correct answer
+	public ArrayList<String> getShortAnswer() {
+		ArrayList<String> questionArray = new ArrayList<String>();
+		int queryToReturn = 0;
+		String question = "";
+		String correctAnswer = "";
+		String query = "Select QUESTION, CORRECT_ANSWER"
+				+ " from ShortAnswer "
 				+ "where QUESTION_ID = ";
 		
 		try {
@@ -198,8 +265,14 @@ public class SQLDatabase {
 					+ "QUESTION text not null,"
 					+ "CORRECT_ANSWER text not null)";
 			
+			String shortAnswer = "Create Table if not exists ShortAnswer"
+					+ "(QUESTION_ID integer primary key autoincrement,"
+					+ "QUESTION text not null,"
+					+ "CORRECT_ANSWER text not null)";
+			
 			curStatement.executeUpdate(multipleChoiceTable);
 			curStatement.executeUpdate(trueFalse);
+			curStatement.executeUpdate(shortAnswer);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
@@ -208,6 +281,7 @@ public class SQLDatabase {
 		
 	}
 	
+	//For testing purposes only
 	public void testTables() {
 		try {
 			this.queryResult = curStatement.executeQuery("Select * from MultipleChoice;");
