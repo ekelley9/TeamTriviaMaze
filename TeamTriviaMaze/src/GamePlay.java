96d5795979af20bbabfa0d6c932036a2c3afa9e2
ekelley9 <57920465+ekelley9@users.noreply.github.com>
2020-06-01 13:44:57 -0700

    Added mazeParser check to the game

--- TeamTriviaMaze/src/GamePlay.java
+++ TeamTriviaMaze/src/GamePlay.java
@@ -12,6 +12,11 @@ public class GamePlay {
 		testMaze.printGameRules();
 
 		while (player.getPlayerRow() != 3 || player.getPlayerCol() != 3) {
+			if(!testMaze.mazeParserHelper(player)) {
+				System.out.println("Looks Like yo don't know as much about Pokemon as you thought"
+						+ "\nthere is no longer a path to the exit you lose!");
+				System.exit(0);
+			}
 			testMaze.printMaze();
 			direction = testMaze.moveSelect();
 			testMaze.move(player, direction);
@@ -21,5 +26,7 @@ public class GamePlay {
 		System.out.println("You win!");
 		testMaze.printMaze();
 	}
+	
+	
 
 }
