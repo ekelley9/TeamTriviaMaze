cce00014ae8664bfaa205ee60deddb5940ab3507
ekelley9 <57920465+ekelley9@users.noreply.github.com>
2020-06-01 13:10:07 -0700

    Added the numberValidator method to TrivaQuestions and used it in the required methods

--- TeamTriviaMaze/src/TriviaQuestions.java
+++ TeamTriviaMaze/src/TriviaQuestions.java
@@ -29,28 +29,22 @@ public class TriviaQuestions {
 	
 	public boolean printTrueOrFalseMenu() {
 		ArrayList<String> trueFalse = database.getTrueFalse();
-		String answer = trueFalse.get(trueFalse.size()-1);
+		String answer = trueFalse.get(trueFalse.size() - 1);
 		String question = trueFalse.get(0);
-		boolean validAnswer = true;
+		boolean isCorrect = true;
 		
 		System.out.println("True or False");
 		System.out.println(question);
 		System.out.println("1. True");
 		System.out.println("2. False");
-		
-		do {
-			int playerAnswer = Integer.parseInt(input.nextLine());
-			if(playerAnswer == 1)
-				return answer.equals("True");
-			else if(playerAnswer == 2)
-				return answer.equals("False");
-			else {
-				System.out.println("Not a valid answer");
-				validAnswer = false;
-			}
-		}while(validAnswer == false);
-		
-		return false;
+
+		int playerAnswer = this.numberValidator(2);
+		if (playerAnswer == 1)
+			isCorrect = answer.equals("True");
+		else if (playerAnswer == 2)
+			isCorrect =  answer.equals("False");
+
+		return isCorrect;
 	}
 	
 	public boolean printMultipleChoiceMenu() {
@@ -65,7 +59,7 @@ public class TriviaQuestions {
 		System.out.println("1. " + multipleChoice.get(0));
 		System.out.println("2. " + multipleChoice.get(1));
 		System.out.println("3. " + multipleChoice.get(2));
-		int playerAnswer = Integer.parseInt(input.nextLine());
+		int playerAnswer = this.numberValidator(3);
 		return answer.equals(multipleChoice.get(playerAnswer-1));
 	}
 	
@@ -79,5 +73,28 @@ public class TriviaQuestions {
 		String playerAnswer = input.nextLine();
 		
 		return answer.equalsIgnoreCase(playerAnswer);
+	}
+	
+	//ensures the input from the user is a number and will not throw an errors
+	public int numberValidator(int maxNum) {
+		boolean isNumber = false;
+		int toReturn = 0;
+		
+		do {
+			try {
+				toReturn = Integer.parseInt(this.input.nextLine());
+				isNumber = true;
+				if(toReturn >= maxNum || toReturn < 1) {
+					System.out.println("Not a valid option please pick a number greater than 0 but less than " + maxNum);
+					isNumber = false;
+				}
+			}catch(NumberFormatException e) {
+				System.out.println("\nSomething went wrong please enter your choice again\n");
+			}
+			
+		}while(!isNumber);
+		
+		return toReturn;
+		
 	}
 }
